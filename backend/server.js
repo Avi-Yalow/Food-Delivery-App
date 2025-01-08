abba2ba02516c601c5d396e172c6fe49c08e9007
AviYalow <avrahamyalow@gmail.com>
2025-01-08 22:54:25 +0200

    backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoutes.js";
+dotenv.config();
+
+// app config
+const app = express();
+const port = process.env.PORT;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+//DB connection
+connectDB();
+
+//api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
