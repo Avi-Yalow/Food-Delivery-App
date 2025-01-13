7c58f6bc7eff20c0fa1dfc048f512caf99360df9
AviYalow <avrahamyalow@gmail.com>
2025-01-13 14:32:31 +0200

    login user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -6,7 +6,28 @@ import userModel from "../models/userModel.js";
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET);
 };
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { password, email } = req.boby;
+  try {
+    const user = userModel.findOne(email);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User does not exists" });
+    }
+    const isMatch = await bycrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    const token = createToken(user._id);
+    return res.status(200).json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: "Error" });
+  }
+};
 const registerUser = async (req, res) => {
   const { name, password, email } = req.boby;
   try {
